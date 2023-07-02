b36438e57f46e696310f5ae4f66146e891773707
Blessed Oscar <93172841+oscar-starks@users.noreply.github.com>
2023-07-02 22:22:21 +0100

    Update views.py

--- home/views.py
+++ home/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "home\index.html")
+    return render(request, "home/index.html")
 
 def about(request):
-    return render(request, 'home\\about.html')
+    return render(request, 'home/about.html')
 
 def contact(request):
-    return render(request, "home\contact.html")
+    return render(request, "home/contact.html")
 
